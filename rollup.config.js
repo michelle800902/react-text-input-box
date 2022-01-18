8b16dd1281e43a2cea0c52ea9ad7e7372b3dee30
Michelle Chen <michelle800902@gmail.com>
2022-01-18 22:19:19 +0800

    Update rollup config to ensure coding style consistently

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from "@rollup/plugin-typescript";
+import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from "rollup-plugin-postcss";
+import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 import del from 'rollup-plugin-delete';
 import dts from 'rollup-plugin-dts';
@@ -28,17 +28,17 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({ tsconfig: './tsconfig.json' }),
       postcss(),
       terser(),
     ],
   },
   {
-    input: "dist/types/index.d.ts",
+    input: 'dist/types/index.d.ts',
     output: [
       {
-        file: "dist/index.d.ts",
-        format: "esm",
+        file: 'dist/index.d.ts',
+        format: 'esm',
       }
     ],
     plugins: [
